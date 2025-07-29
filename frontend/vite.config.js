3998fcc641260a22af4dd639fd8ab333e14ff88b
Mikael Rafael Douw <kokeicreativa@gmail.com>
2025-07-30 02:59:34 +0700

    🚀 Upload versi upgrade: SENTRI

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/sentri-web-app-upgrade/', // ⬅️ GANTI jika nama repo kamu beda
+  base: '/sentri-web-app-upgrade/', // jika nama repo kamu ini
   plugins: [react()],
 })
