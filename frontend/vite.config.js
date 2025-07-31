ef372119667e6b5b3457e03235fc69ed62a021c5
Mikael Rafael Douw <kokeicreativa@gmail.com>
2025-07-31 09:58:02 +0700

    ✅ Fix 404 route React untuk GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/sentri-web-app-upgrade/', // jika nama repo kamu ini
+  base: '/sentri-web-app-upgrade/', // sesuai dengan nama repo kamu
+  build: {
+    outDir: '../docs',
+  },
   plugins: [react()],
 })
